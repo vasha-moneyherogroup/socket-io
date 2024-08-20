c0fc1a15fcd89e2264cad4e97902cf8d5d848e6b
vashafaris <vashafaris@gmail.com>
2024-08-21 04:53:39 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3001;
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('answer', (value) => {
+    socket.emit('answer', value);
+    socket.broadcast.emit('answer', value);
+  });
+
+  socket.on('next', () => {
+    socket.emit('next');
+    socket.broadcast.emit('next');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
